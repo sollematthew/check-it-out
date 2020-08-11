82d521aadb1eb1a4d2ecfab28ad470ca1c76779e
Matt Solle <sollematthew@gmail.com>
2020-08-11 12:36:49 -0400

    feat: remove templated code from App.js

--- App.js
+++ App.js
@@ -64,7 +64,6 @@ const App: () => React$Node = () => {
                 Read the docs to discover what to do next:
               </Text>
             </View>
-            <LearnMoreLinks />
           </View>
         </ScrollView>
       </SafeAreaView>
